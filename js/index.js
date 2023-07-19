cef5b116ae97e1b3732507290dde9be5f926688a
Abner <33882384+jeffabner@users.noreply.github.com>
2023-07-19 11:34:00 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,146 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyC_hxNAiGfW5BC6TXmFsTAbToE0AQwXJxk",
+    authDomain: "autenticacao-a8aba.firebaseapp.com",
+    projectId: "autenticacao-a8aba",
+    storageBucket: "autenticacao-a8aba.appspot.com",
+    messagingSenderId: "611843672712",
+    appId: "1:611843672712:web:0759c99e48f80737f3ad3d",
+    measurementId: "G-RX58KZCWRK"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// Initialize variables
+const auth = firebase.auth()
+const database = firebase.database()
+
+// Set up our register function
+function register () {
+  // Get all our input fields
+  email = document.getElementById('email').value
+  password = document.getElementById('password').value
+  full_name = document.getElementById('full_name').value
+  favourite_song = document.getElementById('favourite_song').value
+  milk_before_cereal = document.getElementById('milk_before_cereal').value
+
+  // Validate input fields
+  if (validate_email(email) == false || validate_password(password) == false) {
+    alert('E-mail ou senha está fora de linha!!')
+    return
+    // Don't continue running the code
+  }
+  if (validate_field(full_name) == false || validate_field(favourite_song) == false || validate_field(milk_before_cereal) == false) {
+    alert('Um ou mais campos extras estão fora da linha!!')
+    return
+  }
+ 
+  // Move on with Auth
+  auth.createUserWithEmailAndPassword(email, password)
+  .then(function() {
+    // Declare user variable
+    var user = auth.currentUser
+
+    // Add this user to Firebase Database
+    var database_ref = database.ref()
+
+    // Create User data
+    var user_data = {
+      email : email,
+      full_name : full_name,
+      favourite_song : favourite_song,
+      milk_before_cereal : milk_before_cereal,
+      last_login : Date.now()
+    }
+
+    // Push to Firebase Database
+    database_ref.child('users/' + user.uid).set(user_data)
+
+    // DOne
+    alert('Usuário Criado!!')
+  })
+  .catch(function(error) {
+    // Firebase will use this to alert of its errors
+    var error_code = error.code
+    var error_message = error.message
+
+    alert(error_message)
+  })
+}
+
+// Set up our login function
+function login () {
+  // Get all our input fields
+  email = document.getElementById('email_login').value
+  password = document.getElementById('password_login').value
+
+  // Validate input fields
+  if (validate_email(email) == false || validate_password(password) == false) {
+    alert('E-mail ou senha está fora de linha!!')
+    return
+    // Don't continue running the code
+  }
+
+  auth.signInWithEmailAndPassword(email, password)
+  .then(function() {
+    // Declare user variable
+    var user = auth.currentUser
+
+    // Add this user to Firebase Database
+    var database_ref = database.ref()
+
+    // Create User data
+    var user_data = {
+      last_login : Date.now()
+    }
+
+    // Push to Firebase Database
+    database_ref.child('users/' + user.uid).update(user_data)
+
+    // DOne
+    alert('Usuário logado!!')
+
+  })
+  .catch(function(error) {
+    // Firebase will use this to alert of its errors
+    var error_code = error.code
+    var error_message = error.message
+
+    alert(error_message)
+  })
+}
+
+
+
+
+// Validate Functions
+function validate_email(email) {
+  expression = /^[^@]+@\w+(\.\w+)+\w$/
+  if (expression.test(email) == true) {
+    // Email is good
+    return true
+  } else {
+    // Email is not good
+    return false
+  }
+}
+
+function validate_password(password) {
+  // Firebase only accepts lengths greater than 6
+  if (password < 6) {
+    return false
+  } else {
+    return true
+  }
+}
+
+function validate_field(field) {
+  if (field == null) {
+    return false
+  }
+
+  if (field.length <= 0) {
+    return false
+  } else {
+    return true
+  }
+}
